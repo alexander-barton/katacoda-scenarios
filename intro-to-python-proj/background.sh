c8265266677daf7f58c4caa55140233c898070c0
Alexander Barton <alexander.barton@camh.ca>
2021-11-11 15:35:23 -0500

    Test if it is apt-get causing the issue.

--- intro-to-python-proj/background.sh
+++ intro-to-python-proj/background.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-apt-get -y update
+#apt-get -y update
+sleep 5
 echo "done" >> /root/apt-update
 
-apt-get -y install python3-venv python3-pip python3-setuptools
-apt-get -y install python3.8-dev python3.8-venv python3.8-distutils
+#apt-get -y install python3-venv python3-pip python3-setuptools
+#apt-get -y install python3.8-dev python3.8-venv python3.8-distutils
+sleep 7
 echo "done" >> /root/apt-install
 
-unlink /usr/bin/python
-ln -s /usr/bin/python3.8 /usr/bin/python
+#unlink /usr/bin/python
+#ln -s /usr/bin/python3.8 /usr/bin/python
+sleep 10
 echo "done" >> /root/tutorial/linking
