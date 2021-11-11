97b18fc89703e4c949d08887c4266fb81ff4d247
Alexander Barton <alexander.barton@camh.ca>
2021-11-11 15:03:31 -0500

    Retry on background script.

--- intro-to-python-proj/background.sh
+++ intro-to-python-proj/background.sh
@@ -5,7 +5,7 @@ export HOME=/root
 apt-get -y update
 echo "done" >> /root/apt-update
 
-apt-get -y install python3-venv
+apt-get -y install python3-venv python3-pip python3-setuptools
 apt-get -y install python3.8-dev python3.8-venv
 echo "done" >> /root/apt-install
 
@@ -13,6 +13,4 @@ unlink /usr/bin/python
 ln -s /usr/bin/python3.8 /usr/bin/python
 echo "done" >> /root/linking
 
-rm /root/linking
-rm /root/apt-install
-rm /root/apt-update
+cd $HOME
