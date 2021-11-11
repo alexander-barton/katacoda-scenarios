f2daf026ebb2a153f14a4cd347517a684468e937
Alexander Barton <alexander.barton@camh.ca>
2021-11-11 13:54:53 -0500

    Another fix...

--- intro-to-python/foreground.sh
+++ intro-to-python/foreground.sh
@@ -2,11 +2,13 @@
 
 {
 echo -n "Initializing environment..."
+
 until [ -e /tmp/ready.txt ]
 do
-echo -n "..."
-sleep 3
+    echo -n "..."
+    sleep 3
 done
+
 echo "Ready!"
 cd /root/notebooks
 } 2>/dev/null
